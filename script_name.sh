9be989ca871e18ba7e1c79b16f2208be10e830e1
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-11-25 10:12:28 +0530

    Update script_name.sh

--- script_name.sh
+++ script_name.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-# Update package repositories and upgrade packages
-sudo apt update && sudo apt upgrade -y
+# Update package repositories
+sudo apt update
 
-# List upgradable packages
-sudo apt list --upgradable
+# Install build essentials
+sudo apt-get install -y build-essential curl wget
 
-# Clean up unused packages and cache
-sudo apt-get autoclean && sudo apt-get autoremove && sudo apt-get clean -y
+# Install additional build and development packages
+sudo apt-get install -y autoconf automake libtool pkg-config cmake git make gcc m4 libltdl7 libltdl-dev libxml2-dev libxml++2.6-dev zlib1g-dev gettext nasm yasm
 
-# Install build essentials and additional development packages
-sudo apt-get install -y build-essential autoconf automake libtool pkg-config cmake git make gcc m4 libltdl7 libltdl-dev libxml2-dev libxml++2.6-dev zlib1g-dev gettext nasm yasm aptitude qtbase5-dev libglib2.0-dev libpcap-dev python3-dev flex asciidoctor libgcrypt20 libgcrypt20-dev
+# Install aptitude
+sudo apt-get install -y aptitude
+
+# Install additional packages using aptitude
+sudo aptitude install -y build-essential qtbase5-dev libglib2.0-dev libpcap-dev python3-dev flex asciidoctor
+sudo apt-get install -y libgcrypt20 libgcrypt20-dev
+
+# Install more packages
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libncurses5-dev automake libtool bison libffi-dev imagemagick libmagickcore-dev libmagickwand-dev libicu-dev
 
 echo "Installation completed!"
+
+# Update and upgrade system
+sudo apt update && sudo apt upgrade -y
+
+# Install additional packages
+sudo apt install -y libipc-run-perl zlib1g apache2-utils apt-transport-https net-tools openssh-server nano curl git mlocate apache2 mysql-server nodejs
+
+echo "All packages installed successfully!"
