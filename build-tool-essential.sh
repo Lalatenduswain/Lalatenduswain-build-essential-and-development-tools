fabf331e819cca45eae859572364f8af1dfe41b3
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2024-06-01 11:30:23 +0530

    Create build-tool-essential.sh

---
+++ build-tool-essential.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Update package repositories
+sudo apt update
+
+# Install build essentials
+sudo apt-get install -y build-essential curl wget
+
+# Install additional build and development packages
+sudo apt-get install -y autoconf automake libtool pkg-config cmake git make gcc m4 libltdl7 libltdl-dev libxml2-dev libxml++2.6-dev zlib1g-dev gettext nasm yasm
+
+# Install aptitude
+sudo apt-get install -y aptitude
+
+# Install additional packages using aptitude
+sudo aptitude install -y build-essential qtbase5-dev libglib2.0-dev libpcap-dev python3-dev flex asciidoctor
+sudo apt-get install -y libgcrypt20 libgcrypt20-dev
+
+# Update package repositories again
+sudo apt-get update
+
+# Install more packages
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libncurses5-dev automake libtool bison libffi-dev imagemagick libmagickcore-dev libmagickwand-dev libicu-dev libaprutil1-dev libapr1-dev
+
+# New packages to install
+sudo apt install -y \
+    libssl-dev \
+    gcc g++ gdb cpp \
+    make cmake \
+    libtool \
+    libc6 \
+    autoconf automake pkg-config \
+    build-essential \
+    gettext \
+    libzstd1 zlib1g \
+    libssh-4 libssh-dev libssl3 \
+    libc6-dev libc6 \
+    libcrypt-dev \
+    netcat lsof wget diffutils
+
+# Update and upgrade system
+sudo apt update && sudo apt upgrade -y
+
+# Install additional packages
+sudo apt install -y libipc-run-perl zlib1g apache2-utils apt-transport-https net-tools openssh-server nano curl git mlocate apache2 mysql-server nodejs
+
+echo "All packages installed successfully!"
